21d025e102017d06e361ea9f3cb0459c2f0ec1c1
Caleb Moore <ctmoore@smu.edu>
2022-04-11 18:09:02 -0500

    asdf

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from itertools import count
+# from itertools import count
+# import streamlit as st
+# import pandas as pd
+
+# if 'count' not in st.session_state:
+#     st.session_state['count'] = 0
+
+# def goUp():
+#     st.session_state['count'] += 1
+
+
+# st.button(str(st.session_state.count), key=None, help=None, on_click=goUp);
+
+# st.write((st.session_state['count']));
+
 import streamlit as st
+import numpy as np
 import pandas as pd
 
+
+df = pd.read_csv('penguins.csv')
+df.head()
+sl= st.slider('Row?', 0, df.index[-1], 25)
+st.title('Penguin data')
 if 'count' not in st.session_state:
-    st.session_state['count'] = 0
+    st.session_state.count = 0
 
-def goUp():
-    st.session_state['count'] += 1
+increment = st.button('Increment')
+if increment:
+    st.session_state.count += 1
 
+st.write('Count = ', st.session_state.count)
 
-st.button(str(st.session_state.count), key=None, help=None, on_click=goUp);
-
-st.write((st.session_state['count']));
+st.write(df.iloc[sl:sl+1])
