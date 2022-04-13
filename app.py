36450905582d0d8f71c868f15a4453bd99a30f24
Caleb Moore <ctmoore@smu.edu>
2022-04-13 16:24:17 -0500

    asdf

--- app.py
+++ app.py
@@ -1,31 +1,23 @@
-# from itertools import count
-# import streamlit as st
-# import pandas as pd
-
-# if 'count' not in st.session_state:
-#     st.session_state['count'] = 0
-
-# def goUp():
-#     st.session_state['count'] += 1
-
-
-# st.button(str(st.session_state.count), key=None, help=None, on_click=goUp);
-
-# st.write((st.session_state['count']));
-
 import streamlit as st
 import numpy as np
 import pandas as pd
+import sqlite3
+con = sqlite3.connect('example.db')
+co = con.cursor()
+
+co.execute("""CREATE TABLE IF NOT EXISTS example(comment text)""")
+
+Lines = co.execute("""SELECT * FROM example""").fetchall()
+
 
 
 df = pd.read_csv('penguins.csv')
-f = open("demo.txt", "a")
+# f = open("demo.txt", "a")
 
-file1 = open('demo.txt', 'r')
-Lines = file1.readlines()
-# df.head()
-# sl= st.slider('Row?', 0, df.index[-1], 25)
-# st.title('Penguin data')
+
+# file1 = open('demo.txt', 'r')
+# Lines = file1.readlines()
+
 if 'count' not in st.session_state:
     st.session_state.count = 0
 
@@ -36,14 +28,13 @@ if increment:
 st.write(df.iloc[0:st.session_state.count+1])
 
 comment = st.text_input("write something")
+co.execute("""INSERT INTO example VALUES(?)""", (comment,))
+# f.write(comment)
+# f.write("\n")
 
-# st.write('comment:', comment)
-f.write(comment)
-f.write("\n")
+# Lines.append(comment)
 
-Lines.append(comment)
-
-count = 0
+# count = 0
 for line in Lines:
     count += 1
     st.write(count, line)
